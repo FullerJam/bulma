2901158852fc96d41a0a7e472706f61a4ce263ff
FullerJam <j.fuller@live.co.uk>
2018-08-06 18:39:06 +0100

    added gulp tasks for sass uglify js and image minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
-
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 /*
     -- TOP LEVEL FUNCTIONS --
     gulp.task - Define tasks
@@ -17,5 +19,28 @@ gulp.task('message', function(){
 gulp.task('copyHtml', function(done){
     gulp.src('src/*.html')
         .pipe(gulp.dest('dist'));
-        done(); // signals async completion with callback function
+        done(); // signals async completion with callback function -> solution found at https://stackoverflow.com/questions/36897877/gulp-error-the-following-tasks-did-not-complete-did-you-forget-to-signal-async
+});
+
+// Optimise images
+gulp.task('imageMin', () =>
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+
+//Minify JS
+gulp.task('minify', function(done){
+        gulp.src('src/js/*.js')
+            .pipe(uglify())
+            .pipe(gulp.dest('dist/js'));
+            done();
+});
+
+//Compile SASS
+gulp.task('sass', function(done){
+    gulp.src('src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+        done();
 });
---
+++ src/js/jsfile.js
@@ -0,0 +1,2 @@
+console.log('line one of code');
+console.log('line two of code');
